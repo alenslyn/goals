b3d014128464978194ae1a1a7d287705ff65355c
alenslyn <alenslyn@gmail.com>
2023-05-19 18:35:34 +0300

    feat: add styledComponents

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,13 +1,36 @@
-import React from 'react';
+import styledComponents from "styled-components";
 
-import './Button.css';
+const Button = styledComponents.button`
+ width: 100%;
+font: inherit;
+  padding: 0.5rem 1.5rem;
+  border: 1px solid rgb(82, 197, 201);
+  color: white;
+  background: rgb(82, 197, 201);
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+  cursor: pointer;
 
-const Button = props => {
-  return (
-    <button type={props.type} className="button" onClick={props.onClick}>
-      {props.children}
-    </button>
-  );
-};
+  @media (min-width: 760px) {
+    width: auto;
+  }
+
+&:focus {
+  outline: none;
+}
+
+&:hover,
+&:active {
+  background: #16a2ac;
+  border-color: #1f989f;
+  box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+}
+`;
+// const Button = props => {
+//   return (
+//     <button type={props.type} className="button" onClick={props.onClick}>
+//       {props.children}
+//     </button>
+//   );
+// };
 
 export default Button;
