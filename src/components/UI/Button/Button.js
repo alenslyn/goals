1fedf5a25f2dd285d15403993c7b48a00d357401
alenslyn <alenslyn@gmail.com>
2023-05-19 18:55:51 +0300

    fix: fix dynamic styles

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,36 +1,40 @@
-import styledComponents from "styled-components";
+import React from "react";
 
-const Button = styledComponents.button`
- width: 100%;
-font: inherit;
-  padding: 0.5rem 1.5rem;
-  border: 1px solid rgb(82, 197, 201);
-  color: white;
-  background: rgb(82, 197, 201);
-  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
-  cursor: pointer;
+import styles from "./Button.module.css";
+// import styled from 'styled-components';
 
-  @media (min-width: 760px) {
-    width: auto;
-  }
+// const Button = styled.button`
+//   width: 100%;
+//   font: inherit;
+//   padding: 0.5rem 1.5rem;
+//   border: 1px solid #8b005d;
+//   color: white;
+//   background: #8b005d;
+//   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+//   cursor: pointer;
 
-&:focus {
-  outline: none;
-}
+//   @media (min-width: 768px) {
+//     width: auto;
+//   }
 
-&:hover,
-&:active {
-  background: #16a2ac;
-  border-color: #1f989f;
-  box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
-}
-`;
-// const Button = props => {
-//   return (
-//     <button type={props.type} className="button" onClick={props.onClick}>
-//       {props.children}
-//     </button>
-//   );
-// };
+//   &:focus {
+//     outline: none;
+//   }
+
+//   &:hover,
+//   &:active {
+//     background: #ac0e77;
+//     border-color: #ac0e77;
+//     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+//   }
+// `;
+
+const Button = (props) => {
+  return (
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
 
 export default Button;
